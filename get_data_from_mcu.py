9931b17aa65a37106fcd9db42d5368480d6764f2
quinnc0 <101291273+quinnc0@users.noreply.github.com>
2022-07-25 13:47:05 -0600

    Add files via upload
    Add arduino program that sends sensor data via USB rather than store it on SD. And add python script to run on PC that communicates with the MCU and receives the sensor data.

---
+++ get_data_from_mcu.py
@@ -0,0 +1,78 @@
+# Establishes connection with microcontroller, takes sensor readings, 
+# and echoes them to terminal (for now)
+
+import sys
+import serial
+import time
+
+arduino = serial.Serial('/dev/ttyUSB0',115200)
+
+def get_poll_interval():
+    poll_interval = 3
+    if(len(sys.argv) > 1):
+        try:
+            poll_interval = float(sys.argv[1])
+        except:
+            print("Invalid argument, using default poll interval.")
+    print("Poll interval is", poll_interval)
+    return poll_interval
+
+def try_read_serial():
+    if(arduino.in_waiting):
+        data = arduino.readline().decode('utf-8').rstrip()
+        return data
+
+def read_serial():
+    while(True):
+        data = try_read_serial()
+        if(type(data) is str):
+            return data
+
+class Ask_For_Data:
+
+    def __init__(self, poll_interval):
+        self._last_time = time.perf_counter()
+        self._is_first_run = True
+        self._poll_interval = poll_interval
+
+    def ask(self):            
+        now_time = time.perf_counter()
+        if (now_time - self._last_time > self._poll_interval or self._is_first_run):
+            if self._is_first_run:
+                self._is_first_run = False
+            self._last_time = now_time
+            arduino.write(bytes("poll", 'utf-8'))
+            return True
+        else:
+            return False
+
+if __name__ == "__main__":
+    a = Ask_For_Data(get_poll_interval())
+
+    # wait for MCU
+    while(True):
+        val = try_read_serial()
+        if (val == "ecs"):
+            # send start command to MCU
+            arduino.write(bytes("go", 'utf-8'))
+            break
+        time.sleep(.2)
+
+    # get column header labels
+    labelstring = read_serial()
+    labels = labelstring.split(", ")
+    print(labelstring)
+
+    while True:
+        # if it's been long enough, ask for another set of readings
+        if(a.ask()):
+            is_first = True
+            for reading in labels:
+                value = read_serial()
+                if is_first:
+                    is_first = False
+                    print(value, end="")
+                else:
+                    print(", " + value, end="") # printing the value
+            print("")
+        
