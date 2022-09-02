2ffb18cdb1a35114675abca9d9bfbae4bc002864
quinnc0 <101291273+quinnc0@users.noreply.github.com>
2022-09-01 18:01:57 -0600

    add argument parser
    adds argument parser for polling interval, sensor log filename, and whether same file should be rewritten each time script is run

--- get_data_from_mcu.py
+++ get_data_from_mcu.py
@@ -4,19 +4,11 @@
 import sys
 import serial
 import time
+import os
+import argparse
 
 LOG_FN = "log.txt"
 arduino = serial.Serial('/dev/ttyUSB0',115200)
-
-def get_poll_interval():
-    poll_interval = 3
-    if(len(sys.argv) > 1):
-        try:
-            poll_interval = float(sys.argv[1])
-        except:
-            print("Invalid argument, using default poll interval.")
-    print("Poll interval is", poll_interval)
-    return poll_interval
 
 def try_read_serial():
     if(arduino.in_waiting):
@@ -28,6 +20,59 @@ def read_serial():
         data = try_read_serial()
         if(type(data) is str):
             return data
+
+def get_next_filename(fn):
+    base_fn, ext_fn = os.path.splitext(fn)
+    i = 1
+    while (os.path.exists(fn)):
+        i_str = "_" + str(i)
+        fn = base_fn + i_str + ext_fn
+        i += 1
+    return fn
+
+# poll_intv : time between sensor polls in seconds
+# log_fn : filename for sensor log
+# overwrite log : bool - either write to same log each time or create new file every time
+def get_data_from_MCU(poll_intv, log_fn, overwrite_log):
+    a = Ask_For_Data(poll_intv)
+
+    if(not overwrite_log):
+        log_fn = get_next_filename(log_fn)
+    f = open(log_fn, "w")
+    f.close()
+
+    # wait for MCU
+    while(True):
+        val = try_read_serial()
+        if (val == "ecs"):
+            # send start command to MCU
+            arduino.write(bytes("go", 'utf-8'))
+            break
+        time.sleep(.2)
+
+    # get column header labels
+    labelstring = read_serial()
+    labels = labelstring.split(", ")
+    print(labelstring)
+    with open(log_fn, "w") as log:
+        log.write("# " + labelstring + "\n")
+
+    while True:
+        # if it's been long enough, ask for another set of readings
+        if(a.ask()):
+            is_first = True
+            for reading in labels:
+                value = read_serial()
+                if is_first:
+                    is_first = False
+                else:
+                    value = ", " + value
+                print(value, end="") # printing the value
+                with open(log_fn, "a") as log:
+                    log.write(value)
+            print("")
+            with open(log_fn, "a") as log:
+                log.write("\n")
 
 class Ask_For_Data:
 
@@ -48,40 +93,9 @@ class Ask_For_Data:
             return False
 
 if __name__ == "__main__":
-    a = Ask_For_Data(get_poll_interval())
-    # overwrite file
-    f = open(LOG_FN, "w")
-    f.close()
-
-    # wait for MCU
-    while(True):
-        val = try_read_serial()
-        if (val == "ecs"):
-            # send start command to MCU
-            arduino.write(bytes("go", 'utf-8'))
-            break
-        time.sleep(.2)
-
-    # get column header labels
-    labelstring = read_serial()
-    labels = labelstring.split(", ")
-    print(labelstring)
-    with open(LOG_FN, "w") as log:
-        log.write("# " + labelstring + "\n")
-
-    while True:
-        # if it's been long enough, ask for another set of readings
-        if(a.ask()):
-            is_first = True
-            for reading in labels:
-                value = read_serial()
-                if is_first:
-                    is_first = False
-                else:
-                    value = ", " + value
-                print(value, end="") # printing the value
-                with open(LOG_FN, "a") as log:
-                    log.write(value)
-            print("")
-            with open(LOG_FN, "a") as log:
-                log.write("\n")
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-f", "--filename", type=str, default="log.txt", help="filename for sensor log output file")
+    ap.add_argument("-i", "--interval", type=int, default=3, help="time in seconds between sensor polls")
+    ap.add_argument("-o", "--overwrite", type=bool, default=False, help="overwrite log file each time or create new")
+    args = vars(ap.parse_args())
+    get_data_from_MCU(args["interval"], args["filename"], args["overwrite"])
